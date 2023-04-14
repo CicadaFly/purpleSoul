cf7261339d28063461a0c81c66027c38b7c0e8ca
CicadaFly <110823334+CicadaFly@users.noreply.github.com>
2023-04-14 18:10:45 +0800

    initial

---
+++ src/components/Cards.jsx
@@ -0,0 +1,7 @@
+const Card = () =>{
+  return(
+    <p> 翻牌區 </p>
+  )
+}
+
+export default Card
---
+++ src/components/Database.jsx
@@ -0,0 +1,18 @@
+import cardBack from '../assets/cardTest.jpg'
+
+const DataBase = [
+  {id:1,name:'1號卡',content:'搞不好是出處',pic:cardBack},
+  {id:2,name:'2號卡',content:'搞不好是出處',pic:cardBack},
+  {id:3,name:'3號卡',content:'搞不好是出處',pic:cardBack},
+  {id:4,name:'4號卡',content:'搞不好是出處',pic:cardBack},
+  {id:5,name:'5號卡',content:'搞不好是出處',pic:cardBack},
+  {id:6,name:'6號卡',content:'搞不好是出處',pic:cardBack},
+  {id:7,name:'7號卡',content:'搞不好是出處',pic:cardBack},
+  {id:8,name:'8號卡',content:'搞不好是出處',pic:cardBack},
+  {id:9,name:'9號卡',content:'搞不好是出處',pic:cardBack},
+  {id:10,name:'10號卡',content:'搞不好是出處',pic:cardBack},
+  {id:11,name:'11號卡',content:'搞不好是出處',pic:cardBack},
+  {id:12,name:'12號卡',content:'搞不好是出處',pic:cardBack},
+]
+
+export default DataBase
