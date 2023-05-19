97d58f1510be8e6546ae60255f2146746082eb51
CicadaFly <110823334+CicadaFly@users.noreply.github.com>
2023-05-19 10:21:05 +0800

    fixing Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
 return(
   <nav className="flex items-center justify-between flex-wrap bg-[#b6c7d1] min-h-[13vh]">
     <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <a href='/'>
+      <a href='/purpleSoul/'>
         <img src={cardImg} alt='Nav_Icon' className="max-h-12 px-3"/>
       </a>
-        <a href='/' className="font-semibold text-xl tracking-wider ml-1 text-zinc-950">
+        <a href='/purpleSoul/' className="font-semibold text-xl tracking-wider ml-1 text-zinc-950">
           洋羽紫靈
           </a>
       <a href="https://www.instagram.com/tarot_celine/" target="_blank" className="absolute right-[3rem]">
